a2590b7be9da0a740260585eb980d3398ad18e05
Ankit Patel <ankit@portworx.com>
2016-01-22 07:22:35 +0000

    Add build script with localrepo setup

---
+++ rpm/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+REV=$1
+if [ "x$REV" == "x" ] ; then
+	echo "Usage: $0 <revision>"
+	exit 1
+fi
+
+LOCALREPO="/etc/yum.repos.d/localrepo.repo"
+if [ ! -f $LOCALREPO ]; then
+    cp localrepo.repo $LOCALREPO
+fi
+yum clean all
+yum repolist | grep localrepo
+
+rm -rf out
+mkdir out
+
+rm -rf px rpmtmp  tmp
+yum -y install --disablerepo="*" --enablerepo="localrepo" kernel-devel-3.10.0-229.14.1.el7.x86_64
+KERNALPATH="/usr/src/kernels/3.10.0-229.14.1.el7.x86_64" VERSION=3.10.0 REVISION=229.14.1.el7.$REV ./buildrpm.sh
+cp -p px/RPMS/x86_64/* out/
+
+rm -rf px rpmtmp  tmp
+yum -y install --disablerepo="*" --enablerepo="localrepo" kernel-ml-devel-3.19.3-1.el7.elrepo.x86_64
+KERNALPATH="/usr/src/kernels/3.19.3-1.el7.elrepo.x86_64" VERSION=3.19.3 REVISION=1.el7.elrepo.$REV ./buildrpm.sh
+cp -p px/RPMS/x86_64/* out/
+
+ls out/
