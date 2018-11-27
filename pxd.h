2e1cf95397daf3a6cdffda43ef9f296bcae2681f
Lakshmi Narasimhan Sundararajan <sulakshm@users.noreply.github.com>
2018-11-27 12:49:26 +0530

    put version back

--- pxd.h
+++ pxd.h
@@ -18,7 +18,7 @@
 #define PXD_DEV  	"pxd/pxd"		/**< block device prefix */
 #define PXD_DEV_PATH	"/dev/" PXD_DEV		/**< block device path prefix */
 
-#define PXD_VERSION 7		/**< driver version */
+#define PXD_VERSION 8		/**< driver version */
 
 #define PXD_NUM_CONTEXTS			11	/**< Total available control devices */
 #define PXD_NUM_CONTEXT_EXPORTED	1	/**< Available for external use */
