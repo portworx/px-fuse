e6b8b1ef5b1b98d18acf4e7923e569f2f5c35820
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2019-07-06 00:41:14 -0700

    ibm mq issue add comment
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- pxd_fastpath.c
+++ pxd_fastpath.c
@@ -817,7 +817,11 @@ int pxd_fastpath_init(struct pxd_device *pxd_dev) {
 			goto fail;
 		}
 
-		// kthread_bind(tc->pxd_thread, i);
+		//
+		// NOTE this has to change for small sized, small queuedepth sync io.
+		// ibm mq issue. Will come in separate PR
+		//
+		kthread_bind(tc->pxd_thread, i);
 		set_user_nice(tc->pxd_thread, MIN_NICE);
 		wake_up_process(tc->pxd_thread);
 	}
