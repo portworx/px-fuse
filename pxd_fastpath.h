47f23347f54a8441bafd0597b76b0cfb9898127d
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2019-09-27 10:11:46 +0530

    revise to support mixed mode repl targets
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- pxd_fastpath.h
+++ pxd_fastpath.h
@@ -32,6 +32,7 @@ struct pxd_io_tracker {
 	struct list_head item; // only HEAD needs this
 	atomic_t active; // only HEAD has refs to all active IO
 	atomic_t fails; // should be zero, non-zero indicates atleast one path failed
+	struct file* file;
 	int read; // if read is from the first target only
 
 	unsigned long start; // start time [HEAD]
@@ -50,7 +51,6 @@ struct thread_context {
 
 struct pxd_fastpath_extension {
 	// Extended information
-	bool   block_device;
 	int bg_flush_enabled; // dynamically enable bg flush from driver
 	int n_flush_wrsegs; // num of PXD_LBS write segments to force flush
 
