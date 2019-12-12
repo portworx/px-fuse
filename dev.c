46c2d99086c2ebfe989f2745ae9c5d35e4d9667a
maxkozlovsky <max@portworx.com>
2019-12-12 13:05:22 -0800

    remove stray printf (#63)

--- dev.c
+++ dev.c
@@ -253,9 +253,6 @@ void fuse_request_send_nowait(struct fuse_conn *fc, struct fuse_req *req)
 	spin_lock(&fc->lock);
 
 	if (fc->connected || fc->allow_disconnected) {
-		if (unlikely(!fc->connected)) {
-			printk(KERN_INFO "%s: Request on disconnected FC", __func__);
-		}
 		fuse_request_send_nowait_locked(fc, req);
 		spin_unlock(&fc->lock);
 
