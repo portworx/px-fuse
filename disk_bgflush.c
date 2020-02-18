1b5656a98c3f4d5f3a57232b317da3513ca6041a
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2020-02-18 20:22:03 +0000

    rework to support multiple kernels
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- disk_bgflush.c
+++ disk_bgflush.c
@@ -3,23 +3,40 @@
 #include <linux/fs.h>
 #include <linux/mm.h>
 #include <linux/timer.h>
+#include <linux/vmstat.h>
 
 #define MAXPOOLS (16)
-static void bgflusher(struct timer_list *unused);
 #define BGFLUSH_CHECK_TIMEOUT (30*HZ)
-static DEFINE_TIMER(bgtimer, bgflusher);
-
+static struct timer_list bgtimer;
 
 static
-bool shouldFlush(void) {
+bool shouldFlush(void)
+{
 	struct sysinfo i;
 	unsigned long pages;
 
+	// installed ram, fetched every cycle to handle ram hot plug cases as well.
 	si_meminfo(&i);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
 	pages = global_node_page_state(LRU_ACTIVE_FILE) + global_node_page_state(LRU_ACTIVE_ANON);
+#else
+	pages = global_page_state(LRU_ACTIVE_FILE) + global_page_state(LRU_ACTIVE_ANON);
+#endif
+	// if active page cache utilization is higher than 50% of installed ram
+	if (pages > i.totalram/2) return true;
 
-	return (i.totalram/2 < pages);
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
+	pages = global_node_page_state(NR_FILE_DIRTY);
+#else
+	pages = global_page_state(NR_FILE_DIRTY);
+#endif
+
+	// if dirty pages are more than 10% of installed ram
+	if (pages > i.totalram/10) return true;
+
+	return false;
 }
 
 static
@@ -35,36 +52,42 @@ struct block_device* init_bdi(struct file *f)
         return I_BDEV(inode);
 }
 
-//static
+static
 void pagecache_flush(void) {
+        const mode_t mode = O_RDONLY;
         struct file *f;
         struct block_device *bdi;
-        int i;
-        const mode_t mode = O_RDONLY;
         char devpath[256];
+        int i;
 
-        for (i=0;i <=MAXPOOLS; i++) {
+	// NOTE currently particular to btrfs implementation
+        for (i = 0;i <= MAXPOOLS; i++) {
+		// look for specific prefix that portworx uses
                 sprintf(devpath, "/var/.px/%d", i);
                 f = filp_open(devpath, mode, 0700);
                 if (IS_ERR_OR_NULL(f)) {
-                        printk("no pool %d found, skipping sync/flush operation\n", i);
+                        // printk("no pool %d found, skipping sync/flush operation\n", i);
                         continue;
                 }
 
                 bdi = init_bdi(f);
-                printk("for pool %d successfully opened %s with block_device %p\n",
-                                i, devpath, bdi);
+                /* printk("for pool %d successfully opened %s with block_device %p\n",
+                                i, devpath, bdi); */
 
                 if (bdi) {
                         fsync_bdev(bdi);
                         invalidate_bdev(bdi);
-                        printk("for pool %d successfully completed sync/flush operation\n", i);
+                        // printk("for pool %d successfully completed sync/flush operation\n", i);
                 }
                 filp_close(f, NULL);
         }
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0)
 static void bgflusher(struct timer_list *unused) {
+#else
+static void bgflusher(unsigned long unused) {
+#endif
 	if (shouldFlush()) 
 		pagecache_flush();
 
@@ -73,8 +96,12 @@ static void bgflusher(struct timer_list *unused) {
 
 
 void init_bgthread(void) {
-	bgtimer.expires = jiffies + BGFLUSH_CHECK_TIMEOUT;
-	add_timer(&bgtimer);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,15,0)
+	timer_setup(&bgtimer, bgflusher, 0);
+#else
+	setup_timer(&bgtimer, bgflusher, 0);
+#endif
+	mod_timer(&bgtimer, jiffies + BGFLUSH_CHECK_TIMEOUT);
 }
 
 void cleanup_bgthread(void) {
