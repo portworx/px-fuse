82202cfd10ec101cd6b789dadbbbf9f67eeec760
prabirpaul <prabir@portworx.com>
2020-07-01 18:44:31 -0700

    Allow greater than 300sec timeout values (#132)

--- pxd_fastpath_stub.c
+++ pxd_fastpath_stub.c
@@ -5,7 +5,6 @@
 #include "pxd_core.h"
 #include "pxd_fastpath.h"
 
-int pxd_device_congested(void *data, int cond) { return 0; }
 int fastpath_init(void) { return 0; }
 void fastpath_cleanup(void) {}
 
@@ -16,7 +15,7 @@ void pxd_fastpath_cleanup(struct pxd_device *pxd_dev) {}
 void pxdctx_set_connected(struct pxd_context *ctx, bool enable) {}
 
 void enableFastPath(struct pxd_device *pxd_dev, bool force) {}
-void disableFastPath(struct pxd_device *pxd_dev) {}
+void disableFastPath(struct pxd_device *pxd_dev, bool force) {}
 int pxd_init_fastpath_target(struct pxd_device *pxd_dev, struct pxd_update_path_out *update_path)
 {
 	// unsupported
@@ -24,4 +23,11 @@ int pxd_init_fastpath_target(struct pxd_device *pxd_dev, struct pxd_update_path_
 	return 0; // cannot fail
 }
 
+void pxd_fastpath_adjust_limits(struct pxd_device *pxd_dev, struct request_queue *topque) {}
+int pxd_suspend_state(struct pxd_device *pxd_dev) {return 0;}
+
+void pxd_suspend_io(struct pxd_device* pxd_dev) { }
+void pxd_resume_io(struct pxd_device* pxd_dev) { }
+int pxd_switch_fastpath(struct pxd_device* pxd_dev) {return -1;}
+int pxd_switch_nativepath(struct pxd_device* pxd_dev) {return -1;}
 #endif
