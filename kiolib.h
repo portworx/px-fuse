bee3f6498b0e066b745208cc6fb994a997b1870e
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2021-02-05 19:23:33 +0530

    cleanup
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- kiolib.h
+++ kiolib.h
@@ -5,11 +5,7 @@ struct file;
 struct pxd_device;
 struct bio;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
 int __do_bio_filebacked(struct pxd_device *pxd_dev, struct bio *bio, struct file *file);
-#else
-int __do_bio_filebacked(struct pxd_device *pxd_dev, struct bio *bio, struct file *file);
-#endif
 
 #endif /* _KIOLIB_H_ */
 
