56c147d994a763ee0f14f6d8c43754cb72ad7d0a
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2021-02-15 12:13:27 +0530

    fix compilation upto 5.9 kernel
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- pxd_fastpath.c
+++ pxd_fastpath.c
@@ -533,7 +533,7 @@ static void pxd_complete_io(struct bio* bio, int error)
 	// debug condition for force fail
 	if (pxd_dev->fp.force_fail) blkrc = -EIO;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,8,1)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,8,0)
 	bio_end_io_acct(bio, iot->start);
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0) || \
     (LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0) &&  \
@@ -1361,6 +1361,7 @@ out_file_failed:
 /* fast path make request function, io entry point */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
 blk_qc_t pxd_make_request_fastpath(struct bio *bio)
+#define BLK_QC_RETVAL BLK_QC_T_NONE
 {
 	struct request_queue *q = bio->bi_disk->queue;
 	struct pxd_device *pxd_dev = bio->bi_disk->private_data;
@@ -1492,7 +1493,7 @@ void pxd_make_request_fastpath(struct request_queue *q, struct bio *bio)
 		return BLK_QC_RETVAL;
 	}
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,8,1)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,8,0)
 	head->start = bio_start_io_acct(bio);
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0) || \
     (LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0) && \
