0a57c08432a0cc14ddb45e6130ad2c615bdad5c8
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2021-02-25 15:08:38 +0530

    using rcu and minor reorg on uniq id alloc use READ/WRITE_ONCE macro to work with connected/allow_disconnected var
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- pxd.c
+++ pxd.c
@@ -88,7 +88,7 @@ static int pxd_open(struct block_device *bdev, fmode_t mode)
 	int err = 0;
 
 	spin_lock(&fc->lock);
-	if (!fc->connected) {
+	if (!READ_ONCE(fc->connected)) {
 		err = -ENXIO;
 	} else {
 		spin_lock(&pxd_dev->lock);
@@ -131,7 +131,7 @@ static long pxd_ioctl_dump_fc_info(void)
 		}
 		printk(KERN_INFO "%s: pxd_ctx: %s ndevices: %lu",
 			__func__, ctx->name, ctx->num_devices);
-		printk(KERN_INFO "\tFC: connected: %d", ctx->fc.connected);
+		printk(KERN_INFO "\tFC: connected: %d", READ_ONCE(ctx->fc.connected));
 	}
 	return 0;
 }
@@ -843,7 +843,7 @@ int pxd_initiate_ioswitch(struct pxd_device *pxd_dev, int code)
 	req->pxd_rdwr_in.size = 0;
 	req->pxd_rdwr_in.flags = PXD_FLAGS_SYNC;
 
-	fuse_request_send_nowait(&pxd_dev->ctx->fc, req, true);
+	fuse_request_send_nowait(&pxd_dev->ctx->fc, req);
 	return 0;
 }
 
@@ -931,7 +931,7 @@ void pxd_reroute_slowpath(struct request_queue *q, struct bio *bio)
 		return;
 	}
 
-	fuse_request_send_nowait(&pxd_dev->ctx->fc, req, true);
+	fuse_request_send_nowait(&pxd_dev->ctx->fc, req);
 }
 
 // fastpath uses this path to punt requests to slowpath
@@ -974,7 +974,7 @@ void pxd_make_request_slowpath(struct request_queue *q, struct bio *bio)
 		return BLK_QC_RETVAL;
 	}
 
-	fuse_request_send_nowait(&pxd_dev->ctx->fc, req, false);
+	fuse_request_send_nowait(&pxd_dev->ctx->fc, req);
 	return BLK_QC_RETVAL;
 }
 
@@ -995,7 +995,7 @@ static void pxd_rq_fn(struct request_queue *q)
 
 		/* Filter out block requests we don't understand. */
 		if (BLK_RQ_IS_PASSTHROUGH(rq) ||
-				(!fc->allow_disconnected && !fc->connected)) {
+				(!READ_ONCE(fc->allow_disconnected) && !READ_ONCE(fc->connected))) {
 			__blk_end_request_all(rq, 0);
 			continue;
 		}
@@ -1030,7 +1030,7 @@ static void pxd_rq_fn(struct request_queue *q)
 			continue;
 		}
 
-		fuse_request_send_nowait(fc, req, false);
+		fuse_request_send_nowait(fc, req);
 		spin_lock_irq(&pxd_dev->qlock);
 	}
 }
@@ -1045,7 +1045,7 @@ static blk_status_t pxd_queue_rq(struct blk_mq_hw_ctx *hctx,
 	struct fuse_conn *fc = &pxd_dev->ctx->fc;
 
 	if (BLK_RQ_IS_PASSTHROUGH(rq) ||
-		(!fc->allow_disconnected && !fc->connected))
+		(!READ_ONCE(fc->allow_disconnected) && !READ_ONCE(fc->connected)))
 		return BLK_STS_IOERR;
 
 	pxd_printk("%s: dev m %d g %lld %s at %ld len %d bytes %d pages "
@@ -1066,7 +1066,7 @@ static blk_status_t pxd_queue_rq(struct blk_mq_hw_ctx *hctx,
 		return BLK_STS_IOERR;
 	}
 
-	fuse_request_send_nowait(fc, req, false);
+	fuse_request_send_nowait(fc, req);
 
 	return BLK_STS_OK;
 }
@@ -1396,7 +1396,6 @@ ssize_t pxd_remove(struct fuse_conn *fc, struct pxd_remove_out *remove)
 	}
 
 	pxd_dev->removing = true;
-	wmb();
 
 	/* Make sure the req_fn isn't called anymore even if the device hangs around */
 	if (pxd_dev->disk && pxd_dev->disk->queue){
@@ -1652,12 +1651,12 @@ ssize_t pxd_timeout_store(struct device *dev, struct device_attribute *attr,
 		return -EINVAL;
 	}
 
-	if (!ctx->fc.connected) {
+	if (!READ_ONCE(ctx->fc.connected)) {
 		cancel_delayed_work_sync(&ctx->abort_work);
 	}
 	spin_lock(&ctx->lock);
 	pxd_timeout_secs = new_timeout_secs;
-	if (!ctx->fc.connected) {
+	if (!READ_ONCE(ctx->fc.connected)) {
 		schedule_delayed_work(&ctx->abort_work, pxd_timeout_secs * HZ);
 	}
 	spin_unlock(&ctx->lock);
@@ -1923,7 +1922,7 @@ static int pxd_nodewipe_cleanup(struct pxd_context *ctx)
 {
 	struct list_head *cur;
 
-	if (ctx->fc.connected) {
+	if (READ_ONCE(ctx->fc.connected)) {
 		return -EINVAL;
 	}
 
@@ -1953,7 +1952,7 @@ static ssize_t pxd_release_store(struct device *dev,
 		printk("pxd kernel node wipe action initiated\n");
 		for (i = 0; i < pxd_num_contexts; ++i) {
 			ctx = &pxd_contexts[i];
-			if (ctx->fc.connected) {
+			if (READ_ONCE(ctx->fc.connected)) {
 				printk("%s px is still connected... cannot release\n", __func__);
 				break;
 			}
@@ -2080,7 +2079,7 @@ static int pxd_control_open(struct inode *inode, struct file *file)
 	}
 
 	fc = &ctx->fc;
-	if (fc->connected == 1) {
+	if (READ_ONCE(fc->connected) == 1) {
 		printk(KERN_ERR "%s: pxd-control-%d(%lld) already open\n", __func__,
 			ctx->id, ctx->open_seq);
 		return -EINVAL;
@@ -2094,14 +2093,13 @@ static int pxd_control_open(struct inode *inode, struct file *file)
 
 	spin_lock(&ctx->lock);
 	pxd_timeout_secs = PXD_TIMER_SECS_DEFAULT;
-	fc->connected = 1;
+	WRITE_ONCE(fc->connected, 1);
 	spin_unlock(&ctx->lock);
 
 	fc->allow_disconnected = 1;
 	file->private_data = fc;
 
 	pxdctx_set_connected(ctx, true);
-	wmb();
 
 	++ctx->open_seq;
 
@@ -2121,11 +2119,10 @@ static int pxd_control_release(struct inode *inode, struct file *file)
 	}
 
 	spin_lock(&ctx->lock);
-	if (ctx->fc.connected == 0) {
+	if (READ_ONCE(ctx->fc.connected) == 0) {
 		pxd_printk("%s: not opened\n", __func__);
 	} else {
-		ctx->fc.connected = 0;
-		wmb();
+		WRITE_ONCE(ctx->fc.connected, 0);
 	}
 
 	schedule_delayed_work(&ctx->abort_work, pxd_timeout_secs * HZ);
@@ -2153,14 +2150,15 @@ static void pxd_abort_context(struct work_struct *work)
 		abort_work);
 	struct fuse_conn *fc = &ctx->fc;
 
-	BUG_ON(fc->connected);
+	BUG_ON(READ_ONCE(fc->connected));
 
 	printk(KERN_ERR "PXD_TIMEOUT (%s:%u): Aborting all requests...",
 		ctx->name, ctx->id);
 
-	fc->allow_disconnected = 0;
+	WRITE_ONCE(fc->allow_disconnected, 0);
+
 	/* Let other threads see the value of allow_disconnected. */
-	wmb();
+	synchronize_rcu();
 
 	fuse_end_queued_requests(fc);
 	pxdctx_set_connected(ctx, false);
@@ -2380,7 +2378,7 @@ void pxd_exit(void)
 
 	for (i = 0; i < pxd_num_contexts; ++i) {
 		/* force cleanup @@@ */
-		pxd_contexts[i].fc.connected = true;
+		WRITE_ONCE(pxd_contexts[i].fc.connected, 1);
 		pxd_context_destroy(&pxd_contexts[i]);
 	}
 
