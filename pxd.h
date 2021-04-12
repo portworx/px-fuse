8b5d801919e27f87caf7080732992bccd2814c70
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2021-04-12 17:31:44 +0530

    support for passing parameters to initialize ioring context

--- pxd.h
+++ pxd.h
@@ -36,6 +36,7 @@
 #define PXD_IOC_UNREGISTER_FILE	_IO(PXD_IOCTL_MAGIC, 8)		/* 0x505808 */
 #define PXD_IOC_FPCLEANUP		_IO(PXD_IOCTL_MAGIC, 9)		/* 0x505809 */
 #define PXD_IOC_IO_FLUSHER		_IO(PXD_IOCTL_MAGIC, 10)	/* 0x50580a */
+#define PXD_IOC_INIT_IO		_IO(PXD_IOCTL_MAGIC, 11)	/* 0x50580A */
 
 #define PXD_MAX_DEVICES	512			/**< maximum number of devices supported */
 #define PXD_MAX_IO		(1024*1024)	/**< maximum io size in bytes */
