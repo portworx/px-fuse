a6dc595b3ea785a84bf4943bb9090860dc6d3129
jrivera-px <jose@portworx.com>
2021-04-15 14:59:18 -0700

    Compilation fix for 4.15.0-141-generic. (#206)

--- pxd_compat.h
+++ pxd_compat.h
@@ -105,7 +105,7 @@
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0) || \
     (LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0) && \
-     defined(blk_queue_fua))
+     defined(bvec_iter_sectors))
 #define QUEUE_FLAG_SET(flag,q) blk_queue_flag_set(flag, q);
 #else
 #define QUEUE_FLAG_SET(flag,q) queue_flag_set_unlocked(flag, q);
