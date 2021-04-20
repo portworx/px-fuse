8bca560d71f8d58a966d451c7ea93cfa39e2ff29
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2021-04-20 13:33:01 +0530

    minor edit
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- pxd.h
+++ pxd.h
@@ -241,7 +241,7 @@ struct pxd_rdwr_in {
 	uint32_t size;		/**< read/write/discard size in bytes */
 	uint32_t flags;		/**< bio flags */
 	uint64_t chksum;	/**< buffer checksum */
-	uint32_t pad; 
+	uint32_t pad;
 	uint64_t offset;	/**< device offset in bytes */
 };
 
