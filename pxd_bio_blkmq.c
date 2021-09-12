ff0d673a8d6e511c837f40adf85e7c51a9e76446
Lakshmi Narasimhan Sundararajan <lns@portworx.com>
2021-09-12 19:35:25 +0530

    use global workqueue
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lns@portworx.com>

--- pxd_bio_blkmq.c
+++ pxd_bio_blkmq.c
@@ -475,13 +475,13 @@ clone_and_map(struct fp_root_context *fproot) {
                         atomic_inc(&pxd_dev->fp.nswitch);
                         if (rq_is_special(rq)) {
                                 INIT_WORK(&cc->work, fp_handle_specialops);
-                                queue_work(pxd_dev->fp.wq, &cc->work);
+                                queue_work(fastpath_workqueue(), &cc->work);
                         } else {
                                 SUBMIT_BIO(clone);
                         }
                 } else {
                         INIT_WORK(&cc->work, pxd_process_fileio);
-                        queue_work(pxd_dev->fp.wq, &cc->work);
+                        queue_work(fastpath_workqueue(), &cc->work);
                 }
         }
 
@@ -543,12 +543,10 @@ static void pxd_io_failover(struct work_struct *work) {
 }
 
 static void pxd_failover_initiate(struct fp_root_context *fproot) {
-        struct pxd_device *pxd_dev = fproot_to_pxd(fproot);
-
         BUG_ON(fproot->magic != FP_ROOT_MAGIC);
 
         INIT_WORK(&fproot->work, pxd_io_failover);
-        queue_work(pxd_dev->fp.wq, &fproot->work);
+        queue_work(fastpath_workqueue(), &fproot->work);
 }
 
 // io handling functions
