2ee428a81cdd05c28c949feb2ea65338ac385364
prabir <prabir@portworx.com>
2022-02-12 14:01:39 -0800

    debug

--- pxd.c
+++ pxd.c
@@ -11,10 +11,6 @@
 #include <linux/bio.h>
 #include <linux/pid_namespace.h>
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,3,0)
-#include <linux/part_stat.h>
-#endif
-
 #define CREATE_TRACE_POINTS
 #undef TRACE_INCLUDE_PATH
 #define TRACE_INCLUDE_PATH .
