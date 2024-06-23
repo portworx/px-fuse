ce7b7dc0f2814043a2f2cd2a55f6a653326bfbf2
Lakshmi Narasimhan Sundararajan <sulakshm@users.noreply.github.com>
2024-06-23 22:33:36 +0530

    PWX-37281: fix tracebacks in kernels during dev add and removals (#317)
    * PWX-36741: Compilation fixes for kernel '5.14.0-427.el9.x86_64' (#313)
    * PWX-36741: Compilation fixes for kernel '5.14.0-427.el9.x86_64'
    
    Signed-off-by: Jose Rivera <jose@portworx.com>
    
    * fix tracebacks in kernels
    
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lsundararajan@purestorage.com>
    ---------
    
    Signed-off-by: Jose Rivera <jose@portworx.com>
    Signed-off-by: Lakshmi Narasimhan Sundararajan <lsundararajan@purestorage.com>
    Co-authored-by: Jose Rivera (Hose) <jose@portworx.com>
    Co-authored-by: Lakshmi Narasimhan Sundararajan <lsundararajan@purestorage.com>

--- pxd.c
+++ pxd.c
@@ -12,6 +12,12 @@
 #include <linux/bio.h>
 #include <linux/pid_namespace.h>
 
+#if defined(RHEL_RELEASE_CODE) && defined(RHEL_RELEASE_VERSION)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0) && RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,4)
+#define __RHEL_GT_94__
+#endif
+#endif
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,3,0) && !defined(part_stat_lock)
 #include <linux/part_stat.h>
 #endif
@@ -92,16 +98,17 @@ struct pxd_context* find_context(unsigned ctx)
 	return &pxd_contexts[ctx];
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0)
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0) || defined(__RHEL_GT_94__)
 static int pxd_open(struct gendisk *bdev, blk_mode_t mode)
 #else
 static int pxd_open(struct block_device *bdev, fmode_t mode)
 #endif
-{	
+{
 	struct pxd_device *pxd_dev;
 	int err = 0;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0)
-	pxd_dev = bdev->private_data;	
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0) || defined(__RHEL_GT_94__)
+	pxd_dev = bdev->private_data;
 #else
 	pxd_dev = bdev->bd_disk->private_data;
 #endif
@@ -127,7 +134,7 @@ static int pxd_open(struct block_device *bdev, fmode_t mode)
 	return err;
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0) || defined(__RHEL_GT_94__)
 static void pxd_release(struct gendisk *disk)
 #else
 static void pxd_release(struct gendisk *disk, fmode_t mode)
@@ -145,7 +152,7 @@ static void pxd_release(struct gendisk *disk, fmode_t mode)
 	BUG_ON(pxd_dev->magic != PXD_DEV_MAGIC);
 	pxd_dev->open_count--;
 	spin_unlock(&pxd_dev->lock);
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,5,0) || defined(__RHEL_GT_94__)
 	trace_pxd_release(pxd_dev->dev_id, pxd_dev->major, pxd_dev->minor);
 #else
 	trace_pxd_release(pxd_dev->dev_id, pxd_dev->major, pxd_dev->minor, mode);
@@ -207,7 +214,7 @@ static long pxd_ioctl_init(struct file *file, void __user *argp)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,0)
 	direction = READ;
 #endif
-	  
+
 	iov_iter_init(&iter, direction, &iov, 1, sizeof(struct pxd_ioctl_init_args));
 
 	return pxd_read_init(&ctx->fc, &iter);
@@ -474,7 +481,7 @@ static void pxd_update_stats(struct fuse_req *req, int rw, unsigned int count)
 {
 		struct pxd_device *pxd_dev = req->queue->queuedata;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,11,0) || defined(__EL8__) 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,11,0) || defined(__EL8__)
 {
 		struct block_device *p = pxd_dev->disk->part0;
 		if (!p) return;
@@ -1208,7 +1215,7 @@ static int pxd_init_disk(struct pxd_device *pxd_dev)
 	 	q = blk_alloc_queue(NUMA_NO_NODE);
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0)
 		q = blk_alloc_queue(pxd_bio_make_request_entryfn, NUMA_NO_NODE);
-#elif LINUX_VERSION_CODE == KERNEL_VERSION(4,18,0) && defined(__EL8__) && defined(QUEUE_FLAG_NOWAIT) 
+#elif LINUX_VERSION_CODE == KERNEL_VERSION(4,18,0) && defined(__EL8__) && defined(QUEUE_FLAG_NOWAIT)
         q = blk_alloc_queue_rh(pxd_bio_make_request_entryfn, NUMA_NO_NODE);
 #else
 		q = blk_alloc_queue(GFP_KERNEL);
@@ -1308,14 +1315,14 @@ static int pxd_init_disk(struct pxd_device *pxd_dev)
 #if defined(QUEUE_FLAG_DISCARD)
 	/* Enable discard support. */
 	QUEUE_FLAG_SET(QUEUE_FLAG_DISCARD,q);
-#endif                                                       
+#endif
 
 #else                                                         // #else for defined(__EL8__) || defined(__SUSE_GT_SP4__)
 	/* Enable discard support. */
 	QUEUE_FLAG_SET(QUEUE_FLAG_DISCARD,q);
 #endif                                                        // #endif for defined(__EL8__) || defined(__SUSE_GT_SP4__)
 #endif                                                        // #endif for LINUX_VERSION_CODE < KERNEL_VERSION(5,19,0)
-	
+
     q->limits.discard_granularity = PXD_MAX_DISCARD_GRANULARITY;
     q->limits.discard_alignment = PXD_MAX_DISCARD_GRANULARITY;
 	if (pxd_dev->discard_size < SECTOR_SIZE)
@@ -1395,18 +1402,6 @@ struct pxd_device* find_pxd_device(struct pxd_context *ctx, uint64_t dev_id)
 
 	return pxd_dev;
 }
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,11,0)
-typedef struct block_device* (*lookup_bdev_wrapper_fn)(char *dev, int mask);
-// This hack is needed because in ubuntu lookup_bdev is defined with 2 arg.
-// ubuntu commit id 6bdf7d686366556020b6ed044fa9eadd090d3984
-// struct block_device *lookup_bdev(const char *pathname, int mask)
-// mask = 0, no perm checks are done
-// So this module shall always push 2 args into stack, but the kernel function
-// decides whether it uses both or only 1.
-// This satisfies the compilation.
-static lookup_bdev_wrapper_fn lookup_bdev_wrapper = (lookup_bdev_wrapper_fn)lookup_bdev;
-#endif
 
 static int __pxd_update_path(struct pxd_device *pxd_dev, struct pxd_update_path_out *update_path);
 ssize_t pxd_add(struct fuse_conn *fc, struct pxd_add_ext_out *add)
@@ -1530,59 +1525,30 @@ ssize_t pxd_export(struct fuse_conn *fc, uint64_t dev_id)
 {
 	struct pxd_context *ctx = container_of(fc, struct pxd_context, fc);
 	struct pxd_device *pxd_dev = find_pxd_device(ctx, dev_id);
-	char devfile[128];
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,11,0)
-	struct block_device *bdev;
-#else
-	dev_t kdev;
-#endif
 	int err = 0;
 
 	if (!pxd_dev) {
 		return -ENOENT;
 	}
 
+	spin_lock(&pxd_dev->lock);
 	if (pxd_dev->exported) {
+		spin_unlock(&pxd_dev->lock);
 		return 0;
 	}
-
-	spin_lock(&pxd_dev->lock);
-    /* pre-check to detect if prior instance is removed */
-    sprintf(devfile, "/dev/pxd/pxd%llu", pxd_dev->dev_id);
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,11,0)
-    bdev = lookup_bdev_wrapper(devfile, 0);
-    if (!IS_ERR(bdev)) {
-		spin_unlock(&pxd_dev->lock);
-    	bdput(bdev);
-    	pr_err("stale bdev %s still alive", devfile);
-    	err = -EEXIST;
-		goto cleanup;
-   }
-#else
-   err = lookup_bdev(devfile, &kdev);
-   if (!err) {
-		spin_unlock(&pxd_dev->lock);
-		pr_err("stale bdev %s still alive", devfile);
-		err = -EEXIST;
-		goto cleanup;
-	}
-#endif
+	spin_unlock(&pxd_dev->lock);
 
 	if (!try_module_get(THIS_MODULE)) {
-		spin_unlock(&pxd_dev->lock);
 		err = -ENODEV;
 		goto cleanup;
 	}
 
-
 	err = pxd_init_disk(pxd_dev);
 	if (err) {
-		spin_unlock(&pxd_dev->lock);
 		module_put(THIS_MODULE);
 		goto cleanup;
 	}
 
-	spin_unlock(&pxd_dev->lock);
 	err = pxd_bus_add_dev(pxd_dev);
 	if (err) {
 		pxd_free_disk(pxd_dev);
@@ -1803,7 +1769,9 @@ ssize_t pxd_read_init(struct fuse_conn *fc, struct iov_iter *iter)
 	struct pxd_device *pxd_dev;
 	struct pxd_init_in pxd_init;
 
-	spin_lock(&ctx->lock);
+	// Taking context lock is unnecessary, as this gets called during
+	// userspace init only once, no changes to the pxd block device
+	// layer is expected during this window.
 
 	pxd_init.num_devices = ctx->num_devices;
 	pxd_init.version = PXD_VERSION;
@@ -1838,9 +1806,8 @@ ssize_t pxd_read_init(struct fuse_conn *fc, struct iov_iter *iter)
 	}
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,0)
-	iter->data_source = WRITE;   // Reset to 'WRITE'  
+	iter->data_source = WRITE;   // Reset to 'WRITE'
 #endif
-	spin_unlock(&ctx->lock);
 
 	printk(KERN_INFO "%s: pxd-control-%d init OK %d devs version %d\n", __func__,
 		ctx->id, pxd_init.num_devices, pxd_init.version);
@@ -1848,7 +1815,6 @@ ssize_t pxd_read_init(struct fuse_conn *fc, struct iov_iter *iter)
 	return copied;
 
 copy_error:
-	spin_unlock(&ctx->lock);
 	return -EFAULT;
 }
 
