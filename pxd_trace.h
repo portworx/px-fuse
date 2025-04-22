bc9af64d04243ffdbb360053dfcf2b0f538b263d
Sebas <sreymondjohnson@purestorage.com>
2025-04-22 19:23:15 +0000

    add whether the bio is single or not, also bio offset in pxd_queue_rq

--- pxd_trace.h
+++ pxd_trace.h
@@ -225,8 +225,8 @@ TRACE_EVENT(
 	pxd_queue_rq,
 	TP_PROTO(uint64_t dev_id, int minor, int dir, uint32_t op,
 		uint64_t offset, uint64_t size, unsigned short nr_phys_segments,
-		uint32_t flags, void *bio, void *bio_tail),
-	TP_ARGS(dev_id, minor, dir, op, offset, size, nr_phys_segments, flags, bio, bio_tail),
+		uint32_t flags, void *bio, void *bio_tail, bool single_bio, uint64_t bio_offset),
+	TP_ARGS(dev_id, minor, dir, op, offset, size, nr_phys_segments, flags, bio, bio_tail, single_bio, bio_offset),
 	TP_STRUCT__entry(
 		__field(uint64_t, dev_id)
 		__field(int, minor)
@@ -238,6 +238,8 @@ TRACE_EVENT(
 		__field(uint32_t, flags)
 		__field(void *, bio)
 		__field(void *, bio_tail)
+		__field(bool, single_bio)
+		__field(uint64_t, bio_offset)
 	),
 	TP_fast_assign(
 		__entry->dev_id = dev_id,
@@ -249,13 +251,15 @@ TRACE_EVENT(
 		__entry->nr_phys_segments = nr_phys_segments,
 		__entry->flags = flags,
 		__entry->bio = bio,
-		__entry->bio_tail = bio_tail
+		__entry->bio_tail = bio_tail,
+		__entry->single_bio = single_bio,
+		__entry->bio_offset = bio_offset
 	),
 	TP_printk(
-		"dev_id %llu minor %d dir %d op %u offset %llu size %llu nr_phys_segments %u flags %x bio %p bio_tail %p",
+		"dev_id %llu minor %d dir %d op %u rq_offset %llu size %llu nr_phys_segments %u flags %x bio %p bio_tail %p single_bio %d bio_offset %llu",
 		__entry->dev_id, __entry->minor, __entry->dir, __entry->op,
 		__entry->offset, __entry->size, __entry->nr_phys_segments,
-		__entry->flags, __entry->bio, __entry->bio_tail)
+		__entry->flags, __entry->bio, __entry->bio_tail, __entry->single_bio, __entry->bio_offset)
 );
 
 TRACE_EVENT(
