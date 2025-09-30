1326286249eae3265758b23d71763fb625d5f407
kagrwl2 <dev.kagrwl@gmail.com>
2025-09-30 09:16:14 +0000

    [PWX-47048] Added device_op_lock to make device detach and io_switch mutually exclusive
    remove delay code
    
    refine to fail immediately if the oplock not acquired for ioswitch, cleanup
    
    remove checks not required

--- pxd_core.h
+++ pxd_core.h
@@ -59,6 +59,8 @@ struct pxd_device {
 	unsigned int nr_congestion_off;
 
 	struct work_struct remove_work;
+	struct completion remove_completion;
+	int remove_result;
 
 	wait_queue_head_t remove_wait;
 	wait_queue_head_t suspend_wq;
