c56759d314c1d3b10a13b0dfd94f2c1fabd20c8a
kagrwl2 <221483402+kagrwl2@users.noreply.github.com>
2026-09-25 14:17:56 +0530

    Compatibility changes for ubuntu and CentOS (#475)
    * ubuntu compatibility fix
    
    centos 7.1, 7.2 kernel compatibility fixes
    
    fix log formatting
    
    igmore alignment flag
    
    remove condition
    
    * use strscpy

--- pxd.c
+++ pxd.c
@@ -11,6 +11,8 @@
 #include <linux/uio.h>
 #include <linux/bio.h>
 #include <linux/pid_namespace.h>
+#include <linux/version.h>
+#include <linux/string.h>
 
 #if defined(RHEL_RELEASE_CODE) && defined(RHEL_RELEASE_VERSION) && defined(__EL8__)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,14,0) && RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9,4)
@@ -188,8 +190,7 @@ static long pxd_ioctl_get_version(void __user *argp)
 		if (sizeof(ver_data) < ver_len) {
 			ver_len = sizeof(ver_data);
 		}
-		strncpy(ver_data, gitversion, ver_len);
-		ver_data[ver_len-1]='\0';
+		pxd_strncpy(ver_data, gitversion, ver_len);
 		if (copy_to_user(argp +
 				 offsetof(struct pxd_ioctl_version_args, piv_len),
 			&ver_len, sizeof(ver_len))) {
--- pxd_fastpath.c
+++ pxd_fastpath.c
@@ -13,6 +13,7 @@
 #include <linux/genhd.h>
 #endif
 #include <linux/workqueue.h>
+#include <linux/string.h>
 
 #include "pxd_bio.h"
 #include "pxd.h"
@@ -544,16 +545,22 @@ void enableFastPath(struct pxd_device *pxd_dev, bool force)
 		fp->file[i] = f;
 
 		inode = file_inode(f);
-		printk(KERN_INFO"device %lld:%d, inode %lu mode %#x\n", pxd_dev->dev_id, i, inode->i_ino, mode);
+		// struct inode::i_ino was widened from unsigned long to u64
+		// (ELRepo 7.1 / mainline late-6.x carries the change). Print as
+		// %llu with an unsigned-long-long cast so the format matches on
+		// both the old and new type without a version guard.
+		printk(KERN_INFO"device %lld:%d, inode %llu mode %#x\n",
+			pxd_dev->dev_id, i, (unsigned long long)inode->i_ino, mode);
 		if (S_ISREG(inode->i_mode)) {
-			printk(KERN_INFO"device[%lld:%d] is a regular file - inode %lu\n",
-					pxd_dev->dev_id, i, inode->i_ino);
+			printk(KERN_INFO"device[%lld:%d] is a regular file - inode %llu\n",
+					pxd_dev->dev_id, i, (unsigned long long)inode->i_ino);
 		} else if (S_ISBLK(inode->i_mode)) {
-			printk(KERN_INFO"device[%lld:%d] is a block device - inode %lu\n",
-				pxd_dev->dev_id, i, inode->i_ino);
+			printk(KERN_INFO"device[%lld:%d] is a block device - inode %llu\n",
+				pxd_dev->dev_id, i, (unsigned long long)inode->i_ino);
 		} else {
-			printk(KERN_INFO"device[%lld:%d] inode %lu unknown device %#x\n",
-				pxd_dev->dev_id, i, inode->i_ino, inode->i_mode);
+			printk(KERN_INFO"device[%lld:%d] inode %llu unknown device %#x\n",
+				pxd_dev->dev_id, i, (unsigned long long)inode->i_ino,
+				inode->i_mode);
 			goto out_file_failed;
 		}
 	}
@@ -713,8 +720,8 @@ int pxd_init_fastpath_target(struct pxd_device *pxd_dev, struct pxd_update_path_
 	for (i = 0; i < update_path->count; i++) {
 		pxd_printk("Fastpath %d(%d): %s, current %s, %px\n", i, pxd_dev->fp.nfd,
 			update_path->devpath[i], pxd_dev->fp.device_path[i], pxd_dev->fp.file[i]);
-		strncpy(pxd_dev->fp.device_path[i], update_path->devpath[i], MAX_PXD_DEVPATH_LEN);
-		pxd_dev->fp.device_path[i][MAX_PXD_DEVPATH_LEN] = '\0';
+		pxd_strncpy(pxd_dev->fp.device_path[i], update_path->devpath[i],
+			sizeof(pxd_dev->fp.device_path[i]));
 		pxd_printk("dev %llu: successfully installed fastpath %s\n",
 			pxd_dev->dev_id, pxd_dev->fp.device_path[i]);
 	}
